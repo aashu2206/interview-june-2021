09422631f0b8fbb2965c59e7dc7af0b8fce4045a
aashu2206 <86254509+aashu2206@users.noreply.github.com>
2021-06-22 13:39:39 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -1,10 +1,24 @@
-package exam;
-
-public class CountCombinations {
-
-  public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
+import java.io.*;
+class GFG
+{
+  static int ways(int n)
+  {
+    if (n == 1)
+    {
+      return 1;
+    }
+    if (n == 2)
+    {
+      return 2;
+    }
+    else
+    {
+      return ways(n - 1) + ways(n - 2);
+    }
   }
-
+    public static void main(String args[])
+    {
+         int n = 5;
+       System.out.println("Number of Ways = " + ways(n));
+    }
 }
